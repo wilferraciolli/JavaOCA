469d40d9da3910e6e956d31d93308ca6607d81c9
Wiliam Ferraciolli <wiliam.ferraciolli@mhr.co.uk>
2018-07-05 08:01:15 +0100

    Initial commit after creating the project

---
+++ src/main/java/com/wiltech/chapter/one/Main.java
@@ -0,0 +1,14 @@
+/**
+ * (c) Midland Software Limited 2018
+ * Name     : Main.java
+ * Author   : ferraciolliw
+ * Date     : 05 Jul 2018
+ */
+package com.wiltech.chapter.one;
+
+/**
+ *
+ */
+public class Main {
+
+}
