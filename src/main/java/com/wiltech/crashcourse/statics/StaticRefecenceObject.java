f2c90280b22094c8111b6c2eaa75d4e831c7806b
Wiliam Ferraciolli <wiliam.ferraciolli@mhr.co.uk>
2018-10-08 18:37:13 +0100

    static and objet reference

--- src/main/java/com/wiltech/crashcourse/statics/StaticRefecenceObject.java
+++ src/main/java/com/wiltech/crashcourse/statics/StaticRefecenceObject.java
@@ -7,17 +7,31 @@
 package com.wiltech.crashcourse.statics;
 
 /**
- *
+ * The type Static refecence object.
  */
 public class StaticRefecenceObject {
 }
 
+/**
+ * The type Thing parent.
+ */
 class ThingParent {
+    /**
+     * The constant x.
+     */
     public static int x = 10;
 }
+
+/**
+ * The type Thingy.
+ */
 class Thingy extends ThingParent {
     private int x =99;
 
+    /**
+     * Do other.
+     * @param t the t
+     */
     public static void doOther(Thingy t){
         System.out.println(t.x);
     }
